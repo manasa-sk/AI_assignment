03acf013b68c26125b249b5e0e740be52a978dea
Manasa SK <manasa.sk.1709@gmail.com>
2023-11-29 17:22:46 +0530

    Task 3

--- client/src/App.js
+++ client/src/App.js
@@ -3,50 +3,83 @@ import axios from 'axios';
 
 function App() {
   const [userQuery, setUserQuery] = useState('');
+  const [botResponse, setBotResponse] = useState('');
+  const [file, setFile] = useState(null);
   const [chatHistory, setChatHistory] = useState([]);
 
   const handleInputChange = (e) => {
     setUserQuery(e.target.value);
   };
 
+  const handleFileChange = (e) => {
+    const uploadedFile = e.target.files[0];
+    setFile(uploadedFile);
+  };
+
   const handleSendQuery = async () => {
     try {
-      const response = await axios.post('http://localhost:3001/api/chat', { query: userQuery });
-      const botResponse = response.data.botResponse;
+      if (file) {
+        // If a PDF file is uploaded, learn from it and then respond to user queries
+        await learnFromPDF(file);
 
-      // Update chat history with the new message
+        // Reset the file state to null after learning
+        setFile(null);
+      }
+
+      // Send user query to the server
+      const response = await axios.post('http://localhost:3001/api/chat', { query: userQuery });
+
+      // Update chat history with the user query and bot response
       setChatHistory((prevHistory) => [
         ...prevHistory,
         { role: 'user', content: userQuery },
-        { role: 'bot', content: botResponse },
+        { role: 'bot', content: response.data.botResponse },
       ]);
 
-      // Clear the input field after sending the query
+      // Update bot response in the state
+      setBotResponse(response.data.botResponse);
       setUserQuery('');
+
     } catch (error) {
       console.error('Error sending query:', error);
     }
   };
 
+  const learnFromPDF = async (pdfFile) => {
+    try {
+      const formData = new FormData();
+      formData.append('pdf', pdfFile);
+
+      // Send the PDF file to the server for learning
+      await axios.post('http://localhost:3001/api/learn', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log('Pdf sent : ', pdfFile);
+    } catch (error) {
+      console.error('Error learning from PDF:', error);
+    }
+  };
+
   return (
     <div>
-      <h1>Chatbot App</h1>
       <div>
-        <div>
-          {chatHistory.map((message, index) => (
-            <div key={index}>
-              <strong>{message.role.charAt(0).toUpperCase() + message.role.slice(1)}:</strong> {message.content}
-            </div>
-          ))}
-        </div>
-        <div>
-          <input type="text" value={userQuery} onChange={handleInputChange} />
-          <button onClick={handleSendQuery}>Send</button>
-        </div>
+        <input type="file" onChange={handleFileChange} />
+      </div>
+      <div>
+        <input type="text" value={userQuery} onChange={handleInputChange} />
+        <button onClick={handleSendQuery}>Send</button>
+      </div>
+      <div>
+        {chatHistory.map((message, index) => (
+          <div key={index}>
+            <strong>{message.role}:</strong> {message.content}
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
 export default App;
-
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const openai = require('openai');
+const pdfParse = require('pdf-parse');
+const multer = require('multer');
 require('dotenv').config();
 
 const app = express();
@@ -13,29 +15,85 @@ app.use(bodyParser.json());
 const openaiApiKey = process.env.OPENAI_API_KEY;
 const openaiClient = new openai.OpenAI({ apiKey: openaiApiKey });
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+let knowledgeBaseEmbeddings = null;
+let pdfText = null;
+let isFileUploaded = false;
+
 app.post('/api/chat', async (req, res) => {
   const userQuery = req.body.query;
 
   try {
-    const botResponse = await generateOpenAIResponse(userQuery);
+    let botResponse = '';
+
+    if (isFileUploaded) {
+      botResponse = await answerQueryWithEmbeddings(userQuery, knowledgeBaseEmbeddings, pdfText);
+    } else {
+      botResponse = await generateResponseWithOpenAI(userQuery);
+    }
+
     res.json({ botResponse });
   } catch (error) {
-    console.error('Error generating OpenAI response:', error);
+    console.error('Error generating response:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-async function generateOpenAIResponse(userQuery) {
-  const prompt = `User: ${userQuery}\nChatGPT:`;
-  const response = await openaiClient.chat.completions.create({
-    messages: [{"role": "user", "content": prompt}],
-    model: "gpt-3.5-turbo",
-  });
+app.post('/api/learn', upload.single('pdf'), async (req, res) => {
+  console.log('Hit learn');
+  try {
+    console.log(req.body);
+    if (req.file) {
+      const pdfBuffer = req.file.buffer;
+      pdfText = await extractTextFromPDF(pdfBuffer);
+      knowledgeBaseEmbeddings = await generateEmbeddings(pdfText);
+      isFileUploaded = true;
+      console.log(isFileUploaded);
+    }
+    res.status(200).json({ message: 'Generated Embeddings' });
+  } catch (error) {
+    console.error('Error generating embeddings:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-  return response.choices[0].message.content;
+async function extractTextFromPDF(pdfBuffer) {
+  const data = await pdfParse(pdfBuffer);
+  console.log('Extracted text from pdf');
+  return data.text;
+}
+
+async function generateEmbeddings(text) {
+  const embedding = await openaiClient.embeddings.create({
+    model: 'text-embedding-ada-002',
+    input: text,
+  });
+  return embedding.data[0].embedding;
+}
+
+async function answerQueryWithEmbeddings(userQuery, knowledgeBaseEmbeddings, pdfText) {
+  const prompt = `Context: ${pdfText} - Question: ${userQuery}`;
+  console.log(prompt);
+  const response = await openaiClient.completions.create({
+    model: 'text-davinci-003',
+    prompt: prompt,
+    max_tokens: 200,
+  });
+  console.log(knowledgeBaseEmbeddings);
+  return response.choices[0].text.trim();
+}
+
+async function generateResponseWithOpenAI(userQuery) {
+  const response = await openaiClient.completions.create({
+    model: 'text-davinci-003',
+    prompt: userQuery,
+    max_tokens: 200,
+  });
+  return response.choices[0].text.trim();
 }
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
